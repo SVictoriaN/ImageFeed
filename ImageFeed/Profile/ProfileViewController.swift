5b8e08e5f2a7ed903f314fc20b875be0e4a30e27
SVictoriaN <syatkina-16@yandex.ru>
2024-11-17 21:19:10 +0300

    Добавила UITabBarController в проект

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -5,11 +5,9 @@ final class ProfileViewController: UIViewController {
     @IBOutlet private var nameLabel: UILabel!
     @IBOutlet private var loginNameLabel: UILabel!
     @IBOutlet private var descriptionLabel: UILabel!
-    
     @IBOutlet private var logoutButton: UIButton!
     
-    
     @IBAction private func didTapLogoutButton() {
+        
     }
-    
 }
