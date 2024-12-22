4528f4fb4d66a9b6cf9e26fb08fb89584407dde6
SVictoriaN <syatkina-16@yandex.ru>
2024-12-22 22:38:17 +0300

    Добавлен экран авторизации с логотипом приложения и кнопкой "Войти". Авторизация реализована с помощью OAuth2.0

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants{
+    static let accessKey = "ABBKs40wIXiDpYdjodrjHd_Qy6OQFpPISMrnyAsm6og"
+    static let secretKey = "WE8va6yDpMFLnKTLDdjptI0uuyIXwJZyb9q_W9D1eM0"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
