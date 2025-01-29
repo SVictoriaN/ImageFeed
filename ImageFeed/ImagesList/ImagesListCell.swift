7491c743792f639ae9de3d2e59b8e4dfc6b3fe38
SVictoriaN <syatkina-16@yandex.ru>
2025-01-29 22:32:32 +0300

    Использование изображений из сети вместо моковых в таблице

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Kingfisher
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
@@ -6,4 +7,13 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet var cellImage: UIImageView!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var likeButton: UIButton!
+    
+    let fullsizeImageView = UIImageView()
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        // Отменяем загрузку, чтобы избежать багов при переиспользовании ячеек
+        fullsizeImageView.kf.cancelDownloadTask()
+    }
 }
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Kingfisher
 
 final class ImagesListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
@@ -7,7 +8,7 @@ final class ImagesListViewController: UIViewController {
     private let currentDate = Date()
     private let showSingleImageSegueIdentifier = "ShowSingleImage"
     private var photos: [Photo] = []
-    private let service = ImagesListService.shared
+    private let imagesListService = ImagesListService.shared
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -16,10 +17,27 @@ final class ImagesListViewController: UIViewController {
         return formatter
     }()
     
+    private var imagesListServiceObserver: NSObjectProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        
+        imagesListService.fetchPhotosNextPage { _ in }
+        
+        imagesListServiceObserver = NotificationCenter.default
+            .addObserver(
+                forName: ImagesListService.didChangeNotification,
+                object: nil,
+                queue: .main
+            ) { [weak self] notification in
+                guard let self = self else { return }
+                print(">>> [ImagesListViewController] Notification received, updating photos")
+                self.updateTableViewAnimated()
+            }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -37,11 +55,40 @@ final class ImagesListViewController: UIViewController {
             super.prepare(for: segue, sender: sender)
         }
     }
+    
+    func updateTableViewAnimated() {
+        let oldCount = photos.count
+        photos = imagesListService.photos // Обновляем массив фотографий из сервиса
+        let newCount = photos.count
+        
+        if oldCount != newCount {
+            tableView.performBatchUpdates {
+                if oldCount < newCount {
+                    let indexPaths = (oldCount..<newCount).map { IndexPath(row: $0, section: 0) }
+                    tableView.insertRows(at: indexPaths, with: .automatic)
+                } else {
+                    let indexPaths = (newCount..<oldCount).map { IndexPath(row: $0, section: 0) }
+                    tableView.deleteRows(at: indexPaths, with: .automatic)
+                }
+            } completion: { _ in }
+        }
+        
+        tableView.reloadData() // Перезагружаем данные таблицы
+    }
+    
+    @objc private func didChangePhotos(notification: Notification) {
+        guard let updatedPhotos = notification.userInfo?["updatedPhotos"] as? [Photo] else { return }
+        
+        tableView.performBatchUpdates {
+            self.photos = updatedPhotos
+            self.tableView.reloadData()
+        }
+    }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photosName.count
+        return photos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -59,12 +106,21 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController {
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
-            return
+        let photo = photos[indexPath.row] // Получаем объект Photo из массива
+        
+        // Используем Kingfisher для загрузки изображения по URL
+        // Преобразование строки в URL
+        if let url = URL(string: photo.thumbImageURL) {
+            cell.cellImage.kf.setImage(with: url, placeholder: UIImage(named: "placeholder"), options: [.transition(.fade(0.2))])
+        } else {
+            // Если URL недействителен, устанавливаем изображение-заглушку
+            cell.cellImage.image = UIImage(named: "placeholder")
         }
         
-        cell.cellImage.image = image
+        // Настройка индикатора загрузки
+        cell.cellImage.kf.indicatorType = .activity
         
+        // Настройка даты
         cell.dateLabel.text = dateFormatter.string(from: currentDate)
         
         let isLiked = indexPath.row % 2 == 0
@@ -80,13 +136,15 @@ extension ImagesListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell,forRowAt indexPath: IndexPath) {
         guard indexPath.row == photos.count - 1 else {return}
-        //service.fetchPhotosNextPage { _ in }
+        imagesListService.fetchPhotosNextPage { _ in }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
-            return 0
-        }
+        //guard let image = UIImage(named: photosName[indexPath.row]) else {
+        //    return 0
+       // }
+        
+        let image = photos[indexPath.row]
         
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
